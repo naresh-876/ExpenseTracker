443820d69b5d53500b375f9e0dffe61ea88ac961
naresh-876 <nareshblueboy12@gmail.com>
2025-10-17 03:39:44 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+package com.rp.expensetracker;
+
+import com.rp.expensetracker.ui.ConsoleApp;
+
+public class Main {
+    // Entry point: starts the console app
+    public static void main(String[] args) {
+        new ConsoleApp().run(); // simple, single line start
+    }
+}
